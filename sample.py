d3ca9c44f70310cc7eb30919dc9e30c1faf6a076
joshbantayan <joshuabantayan31@gmail.com>
2019-05-16 00:13:45 +0800

    sample program for simulation

---
+++ sample.py
@@ -0,0 +1,13 @@
+cycle_input = input("Please input how many cycles:")
+cycles = int(cycle_input)
+redlight = input("Red light duration:")
+greenlight = input("Green light duration:")
+#create list for storing the 4 lanes and these lanes containing
+#the number of cars queued
+for x in range(cycles):
+    print('how many cars are queued:')
+#loop through lanes and add up the number of cars that 
+#passed through the intersection,
+
+#number of cars that passed through should be 
+#approximation or dependent on the number of queued cars?
