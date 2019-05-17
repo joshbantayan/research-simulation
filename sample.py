957db5b15faf341e857eb912d41d39330500837f
joshbantayan <joshuabantayan31@gmail.com>
2019-05-18 04:05:41 +0800

    some sim changes

--- sample.py
+++ sample.py
@@ -1,13 +1,15 @@
+#Sample program with fixed traffic light scheme
 cycle_input = input("Please input how many cycles:")
 cycles = int(cycle_input)
 redlight = input("Red light duration:")
 greenlight = input("Green light duration:")
+abanao = 37.28571429
+harrison = 40.14285714
+magsaysay = 57.42857143
+venice = 36.28571429
+total = 0
 #create list for storing the 4 lanes and these lanes containing
 #the number of cars queued
 for x in range(cycles):
-    print('how many cars are queued:')
-#loop through lanes and add up the number of cars that 
-#passed through the intersection,
-
-#number of cars that passed through should be 
-#approximation or dependent on the number of queued cars?
+    total += abanao + harrison + magsaysay + venice
+print(total)
