c7c27424a67b6d7f07ddb4cfd30fc65b424760fd
joshbantayan <joshuabantayan31@gmail.com>
2019-05-19 23:12:10 +0800

    Added input prompts for each queue

--- sample.py
+++ sample.py
@@ -1,15 +1,19 @@
 #Sample program with fixed traffic light scheme
 cycle_input = input("Please input how many cycles:")
 cycles = int(cycle_input)
-redlight = input("Red light duration:")
-greenlight = input("Green light duration:")
-abanao = 37.28571429
-harrison = 40.14285714
-magsaysay = 57.42857143
-venice = 36.28571429
+redlight = input("Red light duration: ")
+greenlight = input("Green light duration: ")
+
 total = 0
 #create list for storing the 4 lanes and these lanes containing
 #the number of cars queued
 for x in range(cycles):
+    print("Enter number of cars")
+    abanao = int(input("Abanao: " ))
+    harrison = int(input("Harrison: " ))
+    magsaysay = int(input("Magsaysay: " ))
+    venice = int(input("Venice: " ))
     total += abanao + harrison + magsaysay + venice
+    print("")
+    print("-----------")
 print(total)
